e375369ec81b63cc60ea0377534df3a6db6a8688
Gabriel <gabriel.luthun@gmail.com>
2024-05-15 17:31:32 +0200

    feat:datatypes exercises from javascript.info (2.5)

---
+++ javascriptinfo-type-données-2.5-exercices/exercice-1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Énoncé : Quelle est la sortie du script ? */
+
+let name = "Ilya";
+
+console.log(`hello ${1}`);
+
+console.log(`hello ${"name"}`); // ?
+
+console.log(`hello ${name}`); // 
+
+/* Mes réponses : 
+
+1er log : Ilya 
+2ème log : hello name
+3ème log : hello Ilya
+
+*/

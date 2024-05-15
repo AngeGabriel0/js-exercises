28c3c967b948d27688e0e2e13c5e5d3757c9154e
Gabriel <gabriel.luthun@gmail.com>
2024-05-15 17:14:55 +0200

    feat:variables exercises from javascript.info (2.4)

---
+++ javascriptinfo-variables-2.4-exercices/exercice-2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* Énoncé : Créez la variable avec le nom de notre planète. Comment nommeriez-vous une telle variable ?
+Créez la variable pour stocker le nom du visiteur actuel. Comment nommeriez-vous cette variable ? */
+
+let nameOfOurPlanet = "Terre";
+let currentVisitorName = "John";

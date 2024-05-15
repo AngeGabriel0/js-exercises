01de514c3b240cbb26e901a0601683e2babb3014
Gabriel <gabriel.luthun@gmail.com>
2024-05-15 17:42:24 +0200

    feat:if/else JS exercises from javascript.info (2.10)

---
+++ javascriptinfo-ifelse-2.10-exercices/exercice-1.js
@@ -0,0 +1,7 @@
+//Est-ce que alert sera affiché ?
+
+if ("0") {
+    alert('Hello');
+}
+
+//Raisonnement : "0" est une string non vide. Une string non vide est équivalente à true. Donc alert sera affiché.
---
+++ javascriptinfo-ifelse-2.10-exercices/exercice-4.js
@@ -0,0 +1,14 @@
+// Réécrivez ce if en utilisant l’opérateur conditionnel '?' :
+
+let a = 1;
+let b = 2;
+
+if (a + b < 4) {
+    result = 'Below';
+} else {
+    result = 'Over';
+}
+
+//Réponse en ternaire 
+let resultTernaire = a + b < 4 ? 'Below' : 'Over';
+console.log(resultTernaire);
